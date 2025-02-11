4bec6b1777cc52b15f9a03418791b76a65abd3eb
Victor Casas <57921784+vicasas@users.noreply.github.com>
2025-02-10 22:14:44 -0300

    chore(next-config): Remove experimental `typedRoutes` config

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    typedRoutes: true,
-  },
+  /* config options here */
 }
 
 const bundleAnalyzerConfig = withBundleAnalyzer({
