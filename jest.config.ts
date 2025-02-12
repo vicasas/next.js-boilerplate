a8e5517cd4940cf77285b0ac6d6045aa78844e60
Victor Casas <57921784+vicasas@users.noreply.github.com>
2025-02-11 23:42:44 -0300

    feat!: Upgrade Next.js from v14 to v15 (#65)
    Upgrade dependencies to ensure compatibility with Next.js v15:
    
    - **React** updated from v18 to v19.
    - **ESLint** updated from v8 to v9.

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,21 @@
-const nextJest = require('next/jest')
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
-/**
- * Add any custom config to be passed to Jest
- * @type {import('jest').Config}
- */
-const config = {
+// Add any custom config to be passed to Jest
+const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   clearMocks: true,
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/test/e2e/'],
+  testPathIgnorePatterns: ['<rootDir>/test/e2e'],
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   reporters: ['default', ['github-actions', { silent: false }]],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(config)
+export default createJestConfig(config)
