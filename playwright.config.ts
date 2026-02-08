a4b1daaf88214f610ef6d515792cc9d8211d5753
Víctor Casas <57921784+vicasas@users.noreply.github.com>
2026-02-08 10:04:54 -0300

    chore(playwright): Improve e2e config (#134)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,31 +1,32 @@
 import { defineConfig, devices } from '@playwright/test'
+import './infra/loadNextEnv'
 
 const PORT = parseInt(process.env.PORT || '3000', 10)
-const BASE_URL = `http://127.0.0.1:${PORT}`
+const BASE_URL = process.env.PLAYWRIGHT_BASE_URL || `http://127.0.0.1:${PORT}`
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './test/e2e',
-  // Run tests in files in parallel.
+  /* Run tests in files in parallel */
   fullyParallel: true,
-  // Fail the build on CI if you accidentally left test.only in the source code.
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  // Retry on CI only.
+  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  // Opt out of parallel tests on CI.
+  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  // Reporter to use. See https://playwright.dev/docs/test-reporters.
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  // Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions.
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    // Base URL to use in actions like `await page.goto('/')`.
+    /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: BASE_URL,
-    // Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer.
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  // Configure projects for major browsers.
+  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
@@ -39,8 +40,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    // Test against mobile viewports.
+    /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
@@ -49,8 +49,7 @@ export default defineConfig({
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
     // },
-
-    // Test against branded browsers.
+    /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
@@ -60,7 +59,7 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-  // Run your local dev server before starting the tests.
+  /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
     url: BASE_URL,
